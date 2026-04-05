63237bc5c6b449a3935942d642ec489f1bb71661
havinstojijoseph-code <havinstojijoseph@gmail.com>
2026-04-05 13:58:16 +0530

    Add files via upload

---
+++ 20260115-122338.c
@@ -0,0 +1,15 @@
+//havins toji department of biotechnology
+#include<stdio.h>
+void main() 
+{
+       int x, y, n;
+       printf("enter the number of x and y");
+       scanf("%d%d%d", &x, &y, &a);
+       n=x;
+       x=y;
+       y=;
+       printf("value of x is %d", x);
+       printf("value of x is %d", y);
+       printf("value of x after swapping is %d", y);
+        
+}      
